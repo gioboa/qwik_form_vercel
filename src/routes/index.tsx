4346c64ce7e037f4b559dc5b3c6bc1200d10c562
gioboa <giorgiob.boa@gmail.com>
2023-08-26 09:23:07 +0200

    add basic routeAction

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,25 +1,31 @@
-import { component$ } from "@builder.io/qwik";
-import type { DocumentHead } from "@builder.io/qwik-city";
+import { component$ } from '@builder.io/qwik';
+import { routeAction$, type DocumentHead, Form } from '@builder.io/qwik-city';
+
+export const useAddUser = routeAction$(async (user) => {
+	console.log('user', user);
+	return {
+		success: true,
+		userID: Math.random(),
+	};
+});
 
 export default component$(() => {
-  return (
-    <>
-      <h1>Hi 👋</h1>
-      <p>
-        Can't wait to see what you build with qwik!
-        <br />
-        Happy coding.
-      </p>
-    </>
-  );
+	const action = useAddUser();
+	return (
+		<Form action={action}>
+			<input name='name' />
+			<button type='submit'>Add user</button>
+			{action.value?.success && <p>User added successfully</p>}
+		</Form>
+	);
 });
 
 export const head: DocumentHead = {
-  title: "Welcome to Qwik",
-  meta: [
-    {
-      name: "description",
-      content: "Qwik site description",
-    },
-  ],
+	title: 'Welcome to Qwik',
+	meta: [
+		{
+			name: 'description',
+			content: 'Qwik site description',
+		},
+	],
 };
